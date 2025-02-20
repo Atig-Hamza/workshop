9f8e836c5730f453ae2fb230b1bec292abc0b90c
Atig-Hamza <hamzaatig@icloud.com>
2025-02-20 10:47:11 +0100

    display created articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -42,7 +42,7 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        //
+        return view('articles.show', compact('article'));
     }
 
     /**
--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -20,11 +20,28 @@
                 <input type="text" id="title" name="title" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="description">description:</label>
+                <label for="description">Description:</label>
                 <textarea id="description" name="description" class="form-control" required></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Add Article</button>
         </form>
+
+        <!-- Display Articles -->
+        <h2>Articles</h2>
+        <ul class="list-group">
+            @foreach (\App\Models\Article::all() as $article)
+                <li class="list-group-item">
+                    <h3>{{ $article->title }}</h3>
+                    <p>{{ $article->description }}</p>
+                    <a href="/articles.edit" class="btn btn-secondary">Edit</a>
+                    <form action="/articles.destroy" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </li>
+            @endforeach
+        </ul>
     </div>
 </body>
 
